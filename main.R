2d5eef1bbd06fd51ee726a865ab79175c7397a61
pmdemetrio <pablo.demetrio@gmail.com>
2015-12-28 16:28:07 -0300

    inicio

---
+++ main.R
@@ -0,0 +1,13 @@
+# esqueleto
+# Pablo Demetrio
+# December 2015
+# Import packages
+library()
+library()
+# Source functions
+source('R/function1.R')
+source('R/function2.R')
+# Load datasets 
+load('data/input_model.rda')
+
+# Then the actual commands
